3bd522411d3f8b3a308e9e6ac932b184e9070436
Seruy50 <Seruy50@ukr.net>
2023-08-10 14:25:37 +0300

    images

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './styles/main.module.css';
 import RegLogForm from './regAndLogComp/RegLog.jsx';
-import logoMain from './images/logoMain.jpg';
 
 export default function Registration({ logged }){
 
     return <div className={styles.mainBox}>
         <div className={styles.imageBox}>
-        <img src={logoMain} alt="logo"/>
+        <img src={'https://live.staticflickr.com/65535/53105744727_de8e97db51_o.jpg'} alt="logo"/>
         </div>
         <hr className={styles.line}/>
         <RegLogForm logged={logged}/>
